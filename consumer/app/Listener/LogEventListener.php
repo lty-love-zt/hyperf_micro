378f9c95ef7bbbd94abc55bbd7e848ce107879cb
lty <15936061620@163.com>
2020-06-06 23:45:06 +0800

    监听多个事件

--- consumer/app/Listener/LogEventListener.php
+++ consumer/app/Listener/LogEventListener.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace App\Listener;
 
+use App\Event\BeforeUserRegister;
+use App\Event\UserRegistered;
 use Hyperf\Event\Annotation\Listener;
 use Psr\Container\ContainerInterface;
 use Hyperf\Event\Contract\ListenerInterface;
@@ -26,10 +28,18 @@ class LogEventListener implements ListenerInterface
     public function listen(): array
     {
         return [
+            // 监听多个跟用户注册相关的事件
+            BeforeUserRegister::class,
+            UserRegistered::class,
         ];
     }
 
     public function process(object $event)
     {
+        if ($event instanceof BeforeUserRegister) {
+            echo get_class($event) . $event->shouldRegister . PHP_EOL;
+        } elseif ($event instanceof UserRegistered) {
+            echo get_class($event) . $event->userId . PHP_EOL;
+        }
     }
 }
